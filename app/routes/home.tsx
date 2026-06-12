8f6c143019987d7674beb3412366da4910794a05
K Rahul Abhishek <rahul0987abhishek@gmail.com>
2026-06-12 07:46:44 +0530

    updated hero, and upload sections

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
 import Navbar from "../../Components/Navbar";
 import type { Route } from "./+types/home";
+import { Button } from "../../Components/ui/Button";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -10,9 +12,82 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
-      <h1>This is Home</h1>
+    <div className="home">
+      <Navbar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Spacia</p>
+        </div>
+
+        <h1>Build Beautiful Spaces at speed of thought with Spacia</h1>
+
+        <p className="subtitle">
+          Spatia is an  AI first Design Environment that helps you visualize, render and ship architectural projects faster than ever.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">Watch Demo</Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG formats upto 10MB</p>
+            </div>
+
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared Community projects, all in one place</p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="project" />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date('01.01.2026').toLocaleDateString()}</span>
+                    <span>By Rahul Abhi</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
